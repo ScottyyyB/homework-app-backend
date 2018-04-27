47db4260e7969faa22d55d52c8d23ef520d6f1dd
ScottB <17sb@cis.dk>
2018-04-27 12:23:36 +0200

    Ping request spec green

---
+++ spec/requests/api/v0/ping_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Api::V0::PingController, type: :request do
+  describe "/api/v0/ping/index" do
+  	it "should return pong" do
+  	  get "/api/v0/ping/index"
+
+  	  expect(response.status).to eq 200
+  	  expect(JSON.parse(response.body)['message']).to eq "Pong"
+  	end
+  end
+end
