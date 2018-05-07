c18ae820397fba198111edf30490d3aefc52d100
ScottB <17sb@cis.dk>
2018-05-07 15:22:58 +0200

    Create new user model & migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :classrooms, foreign_key: :teacher_id
 
-  validates :name, uniqueness: true
+  validates :username, uniqueness: true
   validates :name, presence: true
   validates :teacher, :student, inclusion: { in: [true, false] }
+  has_secure_password
 end
